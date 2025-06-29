a1c78ede33f9591cf0379e12df181d63a7da6118
Mz-Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-29 22:12:09 +0700

    Aws Service

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,58 +1,33 @@
-data "archive_file" "lambda_hello_world" {
-  type = "zip"
-
-  source_dir  = "${path.module}/hello-world"
-  output_path = "${path.module}/hello-world.zip"
+resource "aws_iam_role" "role" {
+  name               = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_s3_object" "lambda_hello_world" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  key    = "hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
-
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+data "archive_file" "archive" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/index.js"
+  output_path = "${path.module}/lambda/function.zip"
 }
 
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "HelloWorld"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_hello_world.key
+resource "aws_lambda_function" "function" {
+  filename         = data.archive_file.example.output_path
+  function_name    = "example_lambda_function"
+  role             = aws_iam_role.example.arn
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.example.output_base64sha256
 
   runtime = "nodejs20.x"
-  handler = "hello.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+    }
+  }
 
-  role = aws_iam_role.lambda_exec.arn
-}
-
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
-
-  retention_in_days = 30
-}
-
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  tags = {
+    Environment = "production"
+    Application = "example"
+  }
 }
